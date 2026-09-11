d61dcf255ea727b6a2fe279d65febf848819e049
Anurag Bodkhe <bodkheanurag235@gmail.com>
2026-09-12 00:12:49 +0530

    add else if conditions codes

---
+++ Day1/src/ConditionalStatements/Positive_Negative.java
@@ -0,0 +1,13 @@
+package ConditionalStatements;
+
+public class Positive_Negative {
+    public static void main(String[] args){
+        int num = -2;
+
+        if(num>=0){
+            System.out.println("Number is positive");
+        }else{
+            System.out.println("Number is negative");
+        }
+    }
+}
--- Day1/src/ConditionalStatements/if_else_else_if4.java
+++ Day1/src/ConditionalStatements/if_else_else_if4.java
@@ -1,15 +1,17 @@
 package ConditionalStatements;
 
-public class if_else_else_if3 {
+public class if_else_else_if4 {
     public static void main(String[] args){
-        int marks = 35;
+        int marks = 89;
 
-        if(marks>=60){
+        if(marks>=90 && marks<=100){
             System.out.println("A");
-        }else if(marks>=50){
+        }else if(marks>=75 && marks<90){
             System.out.println("B");
-        }else if(marks>=40){
+        }else if(marks>=60 && marks<75){
             System.out.println("C");
+        }else if(marks<=40 && marks<60){
+            System.out.println("D");
         }else{
             System.out.println("Fail");
         }

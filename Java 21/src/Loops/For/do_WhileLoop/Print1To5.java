55eee5a07507b5442bd927b38d4971285237ddd9
Anurag Bodkhe <bodkheanurag235@gmail.com>
2026-09-14 16:57:42 +0530

    push while  loop and do while loop codes

---
+++ Java 21/src/Loops/For/do_WhileLoop/Print1To5.java
@@ -0,0 +1,11 @@
+package Loops.For.do_WhileLoop;
+
+public class Print1To5 {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        } while (i<=5);
+    }
+}
